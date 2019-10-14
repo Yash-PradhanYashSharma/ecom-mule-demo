0ce2f92996760046395891a948076a169c877d8d
Kirti <kirti.gurjar@yash.com>
2019-10-14 18:33:53 +0530

    Updated Invoice PDF file to show Invoice Items

--- sys-ecom-api/src/main/java/com/yash/utility/JsonToPDF.java
+++ sys-ecom-api/src/main/java/com/yash/utility/JsonToPDF.java
@@ -2,12 +2,13 @@ package com.yash.utility;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
-
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
@@ -16,77 +17,92 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 
 public class JsonToPDF {
 
 	public byte[] jsontopdf(String filename, String content) throws IOException {
-
 		Document document = new Document();
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-		try
-
-		{
+		Font f1 = new Font(FontFamily.TIMES_ROMAN, 10.0f, Font.UNDEFINED, BaseColor.BLACK);
+		Font f3 = new Font(FontFamily.TIMES_ROMAN, 14.0f, Font.UNDEFINED, BaseColor.BLACK);
+		try {
 			JSONObject jsonObject = new JSONObject(content);
-
-			PdfPTable table = new PdfPTable(3);
-			table.setWidthPercentage(100);
-			table.setSpacingBefore(10f);
-			table.setSpacingAfter(10f);
-			float[] columnWidths = { 4f, 4f, 4f };
-			table.setWidths(columnWidths);
-			table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-			table.addCell("Total");
-			table.addCell("AmountPaid");
-			table.addCell("AmountDue");
-
-			table.setHeaderRows(1);
-			PdfPCell[] cells = table.getRow(0).getCells();
-			for (int j = 0; j < cells.length; j++) {
-				cells[j].setBackgroundColor(BaseColor.GRAY);
+			PdfPTable table1 = new PdfPTable(3);
+			table1.setWidthPercentage(100);
+			table1.setSpacingBefore(10f);
+			table1.setSpacingAfter(10f);
+			float[] columnWidths1 = { 2f, 2f, 7f };
+			table1.setWidths(columnWidths1);
+			Font f2 = new Font(FontFamily.TIMES_ROMAN, 15.0f, Font.UNDEFINED, BaseColor.BLACK);
+			table1.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+			table1.addCell(new Paragraph("Quantity", f2));
+			table1.addCell(new Paragraph("Unit Cost", f2));
+			table1.addCell(new Paragraph("Description", f2));
+			table1.setHeaderRows(1);
+			PdfPCell[] cells1 = table1.getRow(0).getCells();
+			for (int j = 0; j < cells1.length; j++) {
+				cells1[j].setBackgroundColor(BaseColor.GRAY);
 			}
-			table.addCell(String.valueOf(jsonObject.get("total")));
-			table.addCell(String.valueOf(jsonObject.get("amount_paid")));
-			table.addCell(String.valueOf(jsonObject.get("amount_due")));
-
 			PdfWriter.getInstance(document, out);
-
 			Font f = new Font(FontFamily.TIMES_ROMAN, 30.0f, Font.UNDERLINE, BaseColor.DARK_GRAY);
 			Paragraph p = new Paragraph("Sales Invoice", f);
-
 			p.setAlignment(Paragraph.ALIGN_CENTER);
-
 			document.open();
 			document.add(p);
 			document.add(new Paragraph("\n\n\n"));
-			document.add(new Paragraph("Customer Name: " + (String) jsonObject.get("customer_name")));
-			document.add(new Paragraph("Customer Email: " + (String) jsonObject.get("customer_email")));
-			document.add(new Paragraph("Customer Phone: 7338318134"));
-
+			document.add(new Paragraph("Customer Name: " + (String) jsonObject.get("customer_name"), f3));
+			document.add(new Paragraph("Customer Email: " + (String) jsonObject.get("customer_email"), f3));
+			document.add(new Paragraph("Customer Phone: 7338318134", f3));
+			document.add(new Paragraph("\n\n"));
+			document.add(new Paragraph("Invoice Id: " + (String) jsonObject.get("id"), f3));
+			document.add(new Paragraph("Account Name: " + (String) jsonObject.get("account_name"), f3));
+			document.add(new Paragraph("Account Country: " + (String) jsonObject.get("account_country"), f3));
 			document.add(new Paragraph("\n\n"));
 
-			document.add(new Paragraph("Invoice Id: " + (String) jsonObject.get("id")));
-			document.add(new Paragraph("Account Name: " + (String) jsonObject.get("account_name")));
-			document.add(new Paragraph("Account Country: " + (String) jsonObject.get("account_country")));
-			document.add(new Paragraph("\n\n"));
+			JSONObject jsonobj = jsonObject.getJSONObject("lines");
+			JSONArray jsonarr = jsonobj.getJSONArray("data");
 
-			document.add(table);
+			for (int i = 0; i < jsonarr.length(); i++) {
+				JSONObject json = jsonarr.getJSONObject(i);
+				Iterator<String> keys = json.keys();
+
+				while (keys.hasNext()) {
+					String key = keys.next();
+					if (key.equals("description")) {
+						table1.addCell(new Paragraph((String) json.get("description"), f1));
+					}
+					if (key.equals("amount")) {
+						table1.addCell(new Paragraph(String.valueOf(json.get("amount")), f1));
+					}
+					if (key.equals("quantity")) {
+						table1.addCell(new Paragraph(String.valueOf(json.get("quantity")), f1));
+					}
+				}
+			}
+
+			document.add(table1);
+			document.add(new Paragraph("\n"));
+			document.add(new Paragraph("\n"));
+			Paragraph p1 = new Paragraph("Total: " + String.valueOf(jsonObject.get("total")), f3);
+			p1.setAlignment(Paragraph.ALIGN_RIGHT);
+			document.add(p1);
+			Paragraph p2 = new Paragraph("Amount Paid: " + String.valueOf(jsonObject.get("amount_paid")), f3);
+			p2.setAlignment(Paragraph.ALIGN_RIGHT);
+			document.add(p2);
+			Paragraph p3 = new Paragraph("Amount Due: " + String.valueOf(jsonObject.get("amount_due")), f3);
+			p3.setAlignment(Paragraph.ALIGN_RIGHT);
+			document.add(p3);
+			document.add(new LineSeparator(0.5f, 100, null, 0, -5));
 			document.add(new Paragraph("\n"));
 			document.add(new Paragraph(
-					"Thanks for your purchase! \nFor any futher questions do not hesitate to contact us!"));
-
+					"Thanks for your purchase! \nFor any futher questions do not hesitate to contact us!", f3));
 			document.close();
-
-		} catch (DocumentException e)
-
-		{
-
+		} catch (DocumentException e) {
 			e.printStackTrace();
-
 		}
 		System.out.print("DONEEEEEEE");
 		return out.toByteArray();
-
 	}
 
 }
